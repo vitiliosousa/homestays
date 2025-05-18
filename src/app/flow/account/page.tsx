c62a48a8248009fc9610ee399ccf5f56169f6ca7
Vitilio Sousa <@vitiliosousa>
2025-05-18 10:15:16 +0200

    upload

--- src/app/flow/account/page.tsx
+++ src/app/flow/account/page.tsx
@@ -10,7 +10,6 @@ export default function Account() {
             <Pictures/>
             <div className="relative w-full flex justify-center -mt-16">
                 <div className="relative group">
-
                     {/* Imagem de perfil (metade dentro da capa) */}
                     <div className="relative h-32 w-32 rounded-full border-4 border-slamon overflow-hidden shadow-lg">
                         <Image
@@ -22,7 +21,7 @@ export default function Account() {
                     </div>
                     
                     {/* Ícone de edição */}
-                    <div className="absolute bottom-2 right-2 bg-mint p-2 rounded-full cursor-pointer hover:bg-mint-dark transition-colors group-hover:opacity-100 opacity-0">
+                    <div className="absolute bottom-2 right-2 bg-slamon p-2 rounded-full cursor-pointer hover:bg-mint-dark transition-colors ">
                         <Edit className="h-4 w-4 text-white" />
                     </div>
                 </div>
--- src/components/account/Pictures.tsx
+++ src/components/account/Pictures.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image"
 import cover from "@/assets/cover.svg"
+import { Upload } from "lucide-react"
 
 export default function Pictures() {
-    return(
-        <div className="w-4/5 mt-10">
-            <Image src={cover} alt=""/>
+    return (
+        <div className="w-4/5 h-80 mt-10 relative rounded-lg overflow-hidden shadow-md group">
+            <Image 
+                src={cover}
+                alt="Cover photo"
+                fill
+                className="object-cover object-center pointer-events-none"
+                priority
+            />
+            
+            <button className="absolute bottom-4 right-4 bg-mint hover:bg-salmon text-gray-800 px-4 py-2 rounded-md text-sm font-medium flex items-center gap-2 transition-all shadow-md hover:shadow-lg cursor-pointer z-10">
+                <Upload className="h-4 w-4" />
+                <span className="text-xs">Upload new cover</span>
+            </button>
         </div>
     )
 }
